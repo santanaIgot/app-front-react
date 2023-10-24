ec06a61b6c3348e1b063f437134c135922a0e9bc
santanaIgot <santanaigoor01@gmail.com>
2023-10-24 16:00:00 -0300

    Estrutura modal criada

--- src/components/ModalActions.jsx
+++ src/components/ModalActions.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function ModalActions(props) {
-    return(
-        <>
-
-        </>
-    )
+  if (props) {
+    return 
+    <>
+        
+    </>;
+  }
 }
